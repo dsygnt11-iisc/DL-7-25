599b448c1f166bddd47f2cea0d9e826e933385ee
mohanpanakam <mohanpanakam@gmail.com>
2025-06-25 21:53:00 +0530

    Updated destination into which final model to merge and store

--- Final/Mohan/src/merge_adapter_basemodel.py
+++ Final/Mohan/src/merge_adapter_basemodel.py
@@ -5,7 +5,7 @@ working_dir = "/Users/mohanpanakam/Documents/Finetuning/finetuning"
 
 # Path to your base model and adapter
 base_model_id = "ProsusAI/finbert"
-adapter_path = f"{working_dir}/best_model_fold_2"  # or your adapter directory
+adapter_path = f"{working_dir}/best_model_fold_4"  # or your adapter directory
 
 # Load base model
 base_model = AutoModelForSequenceClassification.from_pretrained(base_model_id, num_labels=3)
@@ -17,5 +17,5 @@ peft_model = PeftModel.from_pretrained(base_model, adapter_path)
 merged_model = peft_model.merge_and_unload()
 
 # Save merged model (now a standard Hugging Face model)
-merged_model.save_pretrained(f"{working_dir}/finbert_best_model_merged", safe_serialization=False)
-AutoTokenizer.from_pretrained(base_model_id).save_pretrained(f"{working_dir}/finbert_best_model_merged")
+merged_model.save_pretrained(f"{working_dir}/goldbert", safe_serialization=False)
+AutoTokenizer.from_pretrained(base_model_id).save_pretrained(f"{working_dir}/goldbert")
